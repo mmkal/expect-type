3dbd308bdec208d3ef1317329dadc1161d5f9d07
Arya Emami <aryaemami59@yahoo.com>
2024-03-18 22:23:17 -0500

    Fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -751,29 +751,25 @@ export type Overloads<T> = T extends {
 }
   ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3, (...args: A4) => R4]
   : T extends {
-      (...args: infer A1): infer R1
-      (...args: infer A2): infer R2
-      (...args: infer A3): infer R3
-    }
-  ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3]
-  : T extends {
-      (...args: infer A1): infer R1
-      (...args: infer A2): infer R2
-    }
-  ? [(...args: A1) => R1, (...args: A2) => R2]
-  : T extends {
-      (...args: infer A1): infer R1
-    }
-  ? [(...args: A1) => R1]
-  : any
+        (...args: infer A1): infer R1
+        (...args: infer A2): infer R2
+        (...args: infer A3): infer R3
+      }
+    ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3]
+    : T extends {
+          (...args: infer A1): infer R1
+          (...args: infer A2): infer R2
+        }
+      ? [(...args: A1) => R1, (...args: A2) => R2]
+      : T extends (...args: infer A1) => infer R1
+        ? [(...args: A1) => R1]
+        : any
 
-export type OverloadedParameters<T> = Overloads<T> extends infer O
-  ? {[K in keyof O]: Parameters<Extract<O[K], (...args: any) => any>>}
-  : never
+export type OverloadedParameters<T> =
+  Overloads<T> extends infer O ? {[K in keyof O]: Parameters<Extract<O[K], (...args: any) => any>>} : never
 
-export type OverloadedReturnType<T> = Overloads<T> extends infer O
-  ? {[K in keyof O]: ReturnType<Extract<O[K], (...args: any) => any>>}
-  : never
+export type OverloadedReturnType<T> =
+  Overloads<T> extends infer O ? {[K in keyof O]: ReturnType<Extract<O[K], (...args: any) => any>>} : never
 
 /**
  * Represents the base interface for the
