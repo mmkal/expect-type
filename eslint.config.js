5a9a95c42730babe25fc09029657fadab9f6044b
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-03-18 22:19:50 -0400

    0.4.0

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,4 @@ const mmkal = require('eslint-plugin-mmkal')
 module.exports = [
   ...mmkal.recommendedFlatConfigs, //
   ...mmkal.configs.globals_jest,
-  {
-    rules: {
-      //   'vitest/expect-expect': 'off',
-      'vitest/expect-expect': ['error', {assertFunctionNames: ['expect', 'expectTypeOf']}],
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-  {
-    files: ['**/*.md/*.ts'],
-    rules: {
-      'no-undef': 'off', // mmkal - babel eslint parser doesn't handle generics properly, might need to figure out how to get typescript-eslint working
-    },
-  },
 ]
