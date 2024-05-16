884d9ac2a3e7c6bb62da41d7813ec6269a695405
Arya Emami <aryaemami59@yahoo.com>
2024-05-16 17:31:00 -0500

    Add JSDocs to for the newly added type helpers

--- src/index.ts
+++ src/index.ts
@@ -783,7 +783,13 @@ export type ExpectTypeOf<Actual, Options extends {positive: boolean}> = Options[
  */
 export type AnyFunction = (...args: any[]) => any
 
-export type Overloads<T> = T extends {
+/**
+ * Extracts a tuple of function types from a given
+ * function type with up to 10 overloads.
+ *
+ * @template FunctionType - The function type to extract overloads from.
+ */
+export type Overloads<FunctionType> = FunctionType extends {
   (...args: infer A1): infer R1
   (...args: infer A2): infer R2
   (...args: infer A3): infer R3
@@ -807,7 +813,7 @@ export type Overloads<T> = T extends {
       (...args: A9) => R9,
       (...args: A10) => R10,
     ]
-  : T extends {
+  : FunctionType extends {
         (...args: infer A1): infer R1
         (...args: infer A2): infer R2
         (...args: infer A3): infer R3
@@ -829,7 +835,7 @@ export type Overloads<T> = T extends {
         (...args: A8) => R8,
         (...args: A9) => R9,
       ]
-    : T extends {
+    : FunctionType extends {
           (...args: infer A1): infer R1
           (...args: infer A2): infer R2
           (...args: infer A3): infer R3
@@ -849,7 +855,7 @@ export type Overloads<T> = T extends {
           (...args: A7) => R7,
           (...args: A8) => R8,
         ]
-      : T extends {
+      : FunctionType extends {
             (...args: infer A1): infer R1
             (...args: infer A2): infer R2
             (...args: infer A3): infer R3
@@ -867,7 +873,7 @@ export type Overloads<T> = T extends {
             (...args: A6) => R6,
             (...args: A7) => R7,
           ]
-        : T extends {
+        : FunctionType extends {
               (...args: infer A1): infer R1
               (...args: infer A2): infer R2
               (...args: infer A3): infer R3
@@ -883,7 +889,7 @@ export type Overloads<T> = T extends {
               (...args: A5) => R5,
               (...args: A6) => R6,
             ]
-          : T extends {
+          : FunctionType extends {
                 (...args: infer A1): infer R1
                 (...args: infer A2): infer R2
                 (...args: infer A3): infer R3
@@ -891,33 +897,44 @@ export type Overloads<T> = T extends {
                 (...args: infer A5): infer R5
               }
             ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3, (...args: A4) => R4, (...args: A5) => R5]
-            : T extends {
+            : FunctionType extends {
                   (...args: infer A1): infer R1
                   (...args: infer A2): infer R2
                   (...args: infer A3): infer R3
                   (...args: infer A4): infer R4
                 }
               ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3, (...args: A4) => R4]
-              : T extends {
+              : FunctionType extends {
                     (...args: infer A1): infer R1
                     (...args: infer A2): infer R2
                     (...args: infer A3): infer R3
                   }
                 ? [(...args: A1) => R1, (...args: A2) => R2, (...args: A3) => R3]
-                : T extends {
+                : FunctionType extends {
                       (...args: infer A1): infer R1
                       (...args: infer A2): infer R2
                     }
                   ? [(...args: A1) => R1, (...args: A2) => R2]
-                  : T extends (...args: infer A1) => infer R1
+                  : FunctionType extends (...args: infer A1) => infer R1
                     ? [(...args: A1) => R1]
                     : any
 
-export type OverloadedParameters<T> =
-  Overloads<T> extends infer O ? {[K in keyof O]: Parameters<Extract<O[K], (...args: any) => any>>} : never
+/**
+ * Extracts a tuple of parameter types from each overload
+ * of a given function type.
+ *
+ * @template FunctionType - The function type to extract parameter types from.
+ */
+export type OverloadedParameters<FunctionType> =
+  Overloads<FunctionType> extends infer O ? {[K in keyof O]: Parameters<Extract<O[K], (...args: any) => any>>} : never
 
-export type OverloadedReturnType<T> =
-  Overloads<T> extends infer O ? {[K in keyof O]: ReturnType<Extract<O[K], (...args: any) => any>>} : never
+/**
+ * Extracts a tuple of return types from each overload of a given function type.
+ *
+ * @template FunctionType - The function type to extract return types from.
+ */
+export type OverloadedReturnType<FunctionType> =
+  Overloads<FunctionType> extends infer O ? {[K in keyof O]: ReturnType<Extract<O[K], (...args: any) => any>>} : never
 
 /**
  * Represents the base interface for the
